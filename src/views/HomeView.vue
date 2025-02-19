44b6d4185bc4d7297ac13063be2babfbd64f41bc
chuhx <chuhx1024@126.com>
2025-02-19 13:55:45 +0800

    Feat: 引入 Antd UI 库

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from 'ant-design-vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Button type="primary">Button</Button>
   </main>
 </template>
