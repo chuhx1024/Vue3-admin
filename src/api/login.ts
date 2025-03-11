5a2cdf586f24eb8d0f779edf5be617602874d280
chuhx <chuhx1024@126.com>
2025-03-11 19:45:52 +0800

    Feat: 调整 login 接口

--- src/api/login.ts
+++ src/api/login.ts
@@ -3,12 +3,12 @@ import { type ILoginRes, type ILoginReq } from '@/api/types/login'
 
 // 用户登录
 export const login = (data: ILoginReq) => {
-    return request<ILoginRes>({
-        method: 'POST',
-        url: '/login',
-        data: new URLSearchParams(data),
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    })
+  return request<ILoginRes>({
+    method: 'POST',
+    url: '/base/login',
+    data: new URLSearchParams(data),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
 }
